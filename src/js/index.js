c4fbb22459673b418479e594e02317c8b05da1ef
Nathalia <163881094+Andreablacm@users.noreply.github.com>
2024-05-21 18:56:34 +0200

    :_))

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+//como se escribe un bucle:
+
+//for (let counter = 0; counter <= 10; counter++) {
+//  console.log(counter);}
+//const names = ['Keko', 'Cris', 'Andrea', 'Celi'];
+//or (let counter = 0; counter < names.length; counter++) {
+//  console.log(names[counter]);
+//}
+
+//for (const name of names) {console.log(name);}
+
+for (let counter = 10; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+const uwuNumbers = [(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)];
+
+for (let counter = 0; counter <= uwuNumbers[]; counter++) {
+  console.log(counter);
+}
